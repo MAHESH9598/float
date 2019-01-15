a47d50cdedb2087b423f5002626852ede9c2b87f
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-15 19:18:16 +0530

    Initial commit

---
+++ float.py
@@ -0,0 +1,23 @@
+#this is Example for ------------>float
+
+""" Definition : --> float is one of the class represented as fundamental data type.
+
+                 --> The purpose of this data type is to store the data,
+                     which is available in the form of Numbers with decimal places.
+
+                 --> with this data type we can represent only decimal number system values,
+                     in the form of integer part and decimal part
+                     but not applicable for Binary, Octal, Hexa decimal number systems.
+
+                 --> with this data type we can represent floating point data which is available in the form
+                     MyateesaExponent or Scientific Notation.
+"""
+
+######### Examples :
+
+a=10.45
+b=12e3
+
+print("The value of 10.45  a =",a,"\t Data type is =",type(a))
+
+print("The value of 12e3   b =",b,"\t Data type is =",type(b))
